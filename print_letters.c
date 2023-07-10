3726ba517db044fa76c92d632f120de7e7949a2f
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-09 20:40:38 -0700

    created print_letters and given initial content

---
+++ print_letters.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_letters - alphabet characters
+ * @list: the valie to be printed
+ *
+ * Return: returns the number of characters printed or -1
+ */
+
+int print_letters(va_list list)
+{
+}
