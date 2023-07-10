f41c1464c462ffcee97c7018ad062ca509c7d3b3
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-10 14:06:42 -0700

    cleaning up the code

--- print_num.c
+++ print_num.c
@@ -17,35 +17,31 @@ int print_int(va_list list)
 	int tempv;
 
 	i = 0;
-	indiv = 0;
+	indiv = 1;
 	tempv = 0;
 	varg3 = va_arg(list, unsigned int);
 	varg3s = varg3;
 	varg33 = varg3;
 	if (varg3)
 	{
-		if (varg3s < 0)
-		{
-			_putchar('-');
-			i++;
-			varg3 = varg3 * -1;
-			varg33 = varg3;
-		}
-		while (varg3 > 9)
-		{
-			varg3 = varg3 / 10;
-			indiv = indiv * 10;
-		}
-		while (indiv > 9)
-		{
-			tempv = varg33 / indiv;
-			_putchar(tempv + 48);
-			varg33 = varg33 % indiv;
-			indiv = indiv / 10;
-			i++;
-		}
-		_putchar(varg33 + 48);
-		return (i);
+		_putchar('-');
+		i++;
+		varg3 = varg3 * -1;
+		varg33 = varg3;
 	}
-	return (-1);
+	while (varg3 > 9)
+	{
+		varg3 = varg3 / 10;
+		indiv = indiv * 10;
+	}
+	while (indiv > 9)
+	{
+		tempv = varg33 / indiv;
+		_putchar(tempv + 48);
+		varg33 = varg33 % indiv;
+		indiv = indiv / 10;
+		i++;
+	}
+	_putchar(varg33 + 48);
+	return (i + 1);
 }
