7a1ddd21ac0837ae0fa45cf8618b70f6943836e4
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-09 20:38:51 -0700

    Created _printf and given initial content

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Function that prints values given with a seperator
+ * @format: the value to be printed
+ *
+ * Return: Returns the number of characters printed or -1
+ */
+
+int _printf(const char * const format, ...)
+{
+}
