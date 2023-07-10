313c99142a5dac444e13dee667649ad0eb986360
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-10 13:03:11 -0700

    cleaned up code and created Format.c

--- _printf.c
+++ _printf.c
@@ -10,18 +10,10 @@
 int _printf(const char * const format, ...)
 {
 	va_list list;
-	int i, i2, cprinted;
+	int i, cprinted;
 	char tempc;
-	true_types stuff[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"d", print_int},
-		{"i", print_int},
-		{"%", NULL},
-		{NULL, NULL},
-	};
 
-	i = 0, i2 = 0, cprinted = 0;
+	i = 0, cprinted = 0;
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
@@ -32,24 +24,13 @@ int _printf(const char * const format, ...)
 		{
 			i++;
 			tempc = format[i];
-			while (i2 < 5)
-			{
-				if (tempc == *stuff[i2].found)
-				{
-					cprinted = cprinted + stuff[i2].doThis(list);
-					i2 = 15;
-				}
-				else
-					i2++;
-			}
-			cprinted = cprinted + print_char2(format[i - 1]);
-			if (i2 == 15)
-				i--;
+			if (tempc == '\0')
+				return (-1);
+			cprinted = cprinted + Format(tempc, list);
 		}
 		else
 			cprinted = cprinted + print_char2(format[i]);
 		i++;
-		i2 = 0;
 	}
 	va_end(list);
 	return (cprinted);
