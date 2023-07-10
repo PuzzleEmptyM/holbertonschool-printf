6b85a0ad0e7f6023a79d6083cbfc366b83c0d76d
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-09 20:42:29 -0700

    created print_num and given initial content

---
+++ print_num.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * print_num - number values to be printed
+ * @list: numbers to print
+ *
+ * Return: returns number of characters printed or -1
+ */
+
+int print_num(va_list list)
+{
+}
